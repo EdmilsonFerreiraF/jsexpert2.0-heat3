e9f2313a72c49227d966a5470d1fd2b6a7f85450
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-04-06 11:12:09 -0300

    🎉 :tada: initial commit 🎉

---
+++ test/todo.test.js
@@ -0,0 +1,11 @@
+const { describe, it, before } = require('mocha')
+const { expect } = require('chai')
+const Todo = require('../src/todo')
+
+describe('todo', () => {
+    describe('#isValid', () => {
+        it('should return invalid when creating an object without text')
+        it('should return invalid when creating an object with invalid "when" property')
+        it('should have "id", "text", "when" and "status" properties after creating object')
+    })
+})
