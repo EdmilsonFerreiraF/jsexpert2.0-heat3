98173d7dc79073d38b4db90b403a98dacc56818b
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-04-06 16:41:26 -0300

    🚀 :rocket: [Add] TodoService - test data format

--- test/todoService.test.js
+++ test/todoService.test.js
@@ -2,6 +2,7 @@ const { describe, it, before, afterEach } = require('mocha')
 const { expect } = require('chai')
 const TodoService = require('../src/todoService')
 const { createSandbox } = require('sinon')
+const Todo = require('../src/todo')
 
 describe('todoService', () => {
     let sandbox
@@ -37,5 +38,40 @@ describe('todoService', () => {
 
             expect(result).to.be.deep.equal([expected])
         })
+    })
+
+    describe('#create', () => {
+        let todoService
+        
+        beforeEach(() => {
+            const dependencies = {
+                todoRepository: {
+                    create: sandbox.stub().returns(true)
+                }
+            }
+
+            todoService = new TodoService(dependencies)
+        })
+
+        it('shouldn\'t save todo item with invalid data', () => {
+            const data = new Todo({
+                text: '',
+                when: ''
+            })
+
+            Reflect.deleteProperty(data, "id")
+
+            const expected = {
+                error: {
+                    message: 'invalid data',
+                    data
+                }
+            }
+
+            const result = todoService.create(data)
+            expect(result).to.be.deep.equal(expected)
+        })
+        it('should save todo item with late \'status\' when it\'s further than today')
+        it('should save todo item with pending status')
     })
 })
