d241425f1681e0325de9d90b251750299bc9afb8
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-04-06 15:59:26 -0300

    🚀 :rocket: [Add] TodoService - test data format

---
+++ test/todoService.test.js
@@ -0,0 +1,41 @@
+const { describe, it, before, afterEach } = require('mocha')
+const { expect } = require('chai')
+const TodoService = require('../src/todoService')
+const { createSandbox } = require('sinon')
+
+describe('todoService', () => {
+    let sandbox
+
+    before(() => {
+        sandbox = createSandbox()
+    })
+
+    describe('#list', () => {
+        const mockDatabase = [
+            {
+                name: 'Marylin Monroe',
+                age: 90,
+                meta: { revision: 0, created: 1649256080370, version: 0 },
+                '$loki': 1
+            }
+        ]
+
+        let todoService
+
+        beforeEach(() => {
+            const dependencies = {
+                todoRepository: {
+                    list: sandbox.stub().returns(mockDatabase)
+                }
+            }
+
+            todoService = new TodoService(dependencies)
+        })
+        it('should return data on a specific format', () => {
+            const result = todoService.list()
+            const [{meta, $loki, ...expected}] = mockDatabase
+
+            expect(result).to.be.deep.equal([expected])
+        })
+    })
+})
